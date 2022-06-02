09dfebe22a795f64fd38ab09fe894d40780a03d7
Akande Oluwatosin <aktosin1@gmail.com>
2022-06-02 20:29:09 +0100

    running the zuri django ennvironment

---
+++ zuridjango/Oluwatosin_Akande/oluwatosin/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class School(models.Model):
+    name = models.CharField(max_length=23)
+    address = models.CharField(max_length=23)
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=23)
